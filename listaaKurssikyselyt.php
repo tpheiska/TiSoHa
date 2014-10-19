cd03f9182b0957faafa0345dff80019d1c6429d5
tpheiska <tpheiska@saunalahti.fi>
2014-10-20 01:12:03 +0300

    Delete listaaKurssikyselyt.php

--- listaaKurssikyselyt.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once 'libs/functions.php';
-    require_once 'libs/models/kurssikyselyt.php';
-
-    naytaNakyma('index', array('kurssikyselyt' => Kurssikysely::etsiAktiivisetKurssikyselyt()));
